6110276657510db8bcc4991ff23d30fcd9d5966f
TheLordXII <56317585+TheLordXII@users.noreply.github.com>
2021-01-29 19:22:24 +0100

    Addwf UnitTest erstellt

--- UnitTests/OperationenUnitTestBYTE.cs
+++ UnitTests/OperationenUnitTestBYTE.cs
@@ -6,8 +6,26 @@ using System.Text;
 namespace Simulator
 {
     [TestClass]
-    class OperationenUnitTestBYTE
+    public class OperationenUnitTestBYTE
     {
+        [TestMethod]
+        public void TestOperationen_addwf()
+        {
+            //Arrange 
+            Operationen operation = new Operationen();
+            int programminhalt = 1793;
+            int programmcounter = 1;
+            operation.w = 1;
+            operation.Bank1[1] = 1;
+
+            //Act
+            int NeuerProgrammcounter = operation.addwf(programminhalt, programmcounter);
+
+            //Assert
+            Assert.AreEqual(2, operation.w);
+            Assert.AreEqual(1, NeuerProgrammcounter);
+        }
+
 
     }
 }
