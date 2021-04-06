b43c61f2c197f72323aacae26c5514d2795fc136
TheLordXII <56317585+TheLordXII@users.noreply.github.com>
2021-04-06 11:17:09 +0200

    Refactoring nichtssagende Variablennamen

--- Simulator/Models/Controls/OperationSelector.cs
+++ Simulator/Models/Controls/OperationSelector.cs
@@ -10,12 +10,10 @@ namespace Simulator.Models.Controls
 {
     class OperationSelector
     {
-        private Memory _memory;
         private CommandSet _commandSet;
 
-        public OperationSelector(Memory memory, CommandSet commandSet)
+        public OperationSelector(CommandSet commandSet)
         {
-            _memory = memory;
             _commandSet = commandSet;
         }
 
@@ -31,107 +29,107 @@ namespace Simulator.Models.Controls
             {
                 //BYTE-ORIENTED FILE REGISTER OPERATIONS
 
-                case short n when (0b00_0111_0000_0000 <= n && n <= 0b00_0111_1111_1111):
+                case short opCodeRange when (0b00_0111_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0111_1111_1111):
                     _commandSet._byteCommands.ADDWF(register, destination);
                     break;
 
-                case short n when (0b00_0101_0000_0000 <= n && n <= 0b00_0101_1111_1111):
+                case short opCodeRange when (0b00_0101_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0101_1111_1111):
                     _commandSet._byteCommands.ANDWF(register, destination);
                     break;
 
-                case short n when (0b00_0001_1000_0000 <= n && n <= 0b00_0001_1111_1111):
+                case short opCodeRange when (0b00_0001_1000_0000 <= opCodeRange && opCodeRange <= 0b00_0001_1111_1111):
                     _commandSet._byteCommands.CLRF(register);
                     break;
 
-                case short n when (0b00_0001_0000_0000 <= n && n <= 0b00_0001_0111_1111):
+                case short opCodeRange when (0b00_0001_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0001_0111_1111):
                     _commandSet._byteCommands.CLRW();
                     break;
 
-                case short n when (0b00_1001_0000_0000 <= n && n <= 0b00_1001_1111_1111):
+                case short opCodeRange when (0b00_1001_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1001_1111_1111):
                     _commandSet._byteCommands.COMF(register, destination);
                     break;
 
-                case short n when (0b00_0011_0000_0000 <= n && n <= 0b00_0011_1111_1111):
+                case short opCodeRange when (0b00_0011_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0011_1111_1111):
                     _commandSet._byteCommands.DECF(register, destination);
                     break;
 
-                case short n when (0b00_1011_0000_0000 <= n && n <= 0b00_1011_1111_1111):
+                case short opCodeRange when (0b00_1011_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1011_1111_1111):
                     _commandSet._byteCommands.DECFSZ(register, destination);
                     break;
 
-                case short n when (0b00_1010_0000_0000 <= n && n <= 0b00_1010_0000_0000):
+                case short opCodeRange when (0b00_1010_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1010_0000_0000):
                     _commandSet._byteCommands.INCF(register, destination);
                     break;
 
-                case short n when (0b00_1111_0000_0000 <= n && n <= 0b00_1111_1111_1111):
+                case short opCodeRange when (0b00_1111_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1111_1111_1111):
                     _commandSet._byteCommands.INCFSZ(register, destination);
                     break;
 
-                case short n when (0b00_0100_0000_0000 <= n && n <= 0b00_0100_1111_1111):
+                case short opCodeRange when (0b00_0100_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0100_1111_1111):
                     _commandSet._byteCommands.IORWF(register, destination);
                     break;
 
-                case short n when (0b00_1000_0000_0000 <= n && n <= 0b00_1000_1111_1111):
+                case short opCodeRange when (0b00_1000_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1000_1111_1111):
                     _commandSet._byteCommands.MOVF(register, destination);
                     break;
 
-                case short n when (0b00_0000_1000_0000 <= n && n <= 0b00_0000_1111_1111):
+                case short opCodeRange when (0b00_0000_1000_0000 <= opCodeRange && opCodeRange <= 0b00_0000_1111_1111):
                     _commandSet._byteCommands.MOVWF(register);
                     break;
 
-                case short n when (n == 0b00_0000_0000_0000 || n == 0b00_0000_0110_0000 || n == 0b00_0000_0010_0000 || n == 0b00_0000_0100_0000):
+                case short opCodeRange when (opCodeRange == 0b00_0000_0000_0000 || opCodeRange == 0b00_0000_0110_0000 || opCodeRange == 0b00_0000_0010_0000 || opCodeRange == 0b00_0000_0100_0000):
                     _commandSet._byteCommands.NOP();
                     break;
 
-                case short n when (0b00_1101_0000_0000 <= n && n <= 0b00_1101_1111_1111):
+                case short opCodeRange when (0b00_1101_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1101_1111_1111):
                     _commandSet._byteCommands.RLF(register, destination);
                     break;
 
-                case short n when (0b00_1100_0000_0000 <= n && n <= 0b00_1100_1111_1111):
+                case short opCodeRange when (0b00_1100_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1100_1111_1111):
                     _commandSet._byteCommands.RRF(register, destination);
                     break;
 
-                case short n when (0b00_0010_0000_0000 <= n && n <= 0b00_0010_1111_1111):
+                case short opCodeRange when (0b00_0010_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0010_1111_1111):
                     _commandSet._byteCommands.SUBWF(register, destination);
                     break;
 
-                case short n when (0b00_1110_0000_0000 <= n && n <= 0b00_1110_1111_1111):
+                case short opCodeRange when (0b00_1110_0000_0000 <= opCodeRange && opCodeRange <= 0b00_1110_1111_1111):
                     _commandSet._byteCommands.SWAPF(register, destination);
                     break;
 
-                case short n when (0b00_0110_0000_0000 <= n && n <= 0b00_0110_1111_1111):
+                case short opCodeRange when (0b00_0110_0000_0000 <= opCodeRange && opCodeRange <= 0b00_0110_1111_1111):
                     _commandSet._byteCommands.XORWF(register, destination);
                     break;
 
                 //BIT-ORIENTED FILE REGISTER OPERATIONS
 
-                case short n when (0b01_0000_0000_0000 <= n && n <= 0b01_0011_1111_1111):
+                case short opCodeRange when (0b01_0000_0000_0000 <= opCodeRange && opCodeRange <= 0b01_0011_1111_1111):
                     _commandSet._bitCommands.BCF(register, bit);
                     break;
 
-                case short n when (0b01_0100_0000_0000 <= n && n <= 0b01_0111_1111_1111):
+                case short opCodeRange when (0b01_0100_0000_0000 <= opCodeRange && opCodeRange <= 0b01_0111_1111_1111):
                     _commandSet._bitCommands.BSF(register, bit);
                     break;
 
-                case short n when (0b01_1000_0000_0000 <= n && n <= 0b01_1011_1111_1111):
+                case short opCodeRange when (0b01_1000_0000_0000 <= opCodeRange && opCodeRange <= 0b01_1011_1111_1111):
                     _commandSet._bitCommands.BTFSC(register, bit);
                     break;
 
-                case short n when (0b01_1100_0000_0000 <= n && n <= 0b01_1111_1111_1111):
+                case short opCodeRange when (0b01_1100_0000_0000 <= opCodeRange && opCodeRange <= 0b01_1111_1111_1111):
                     _commandSet._bitCommands.BTFSS(register, bit);
                     break;
 
                 //LITERAL AND CONTROL OPERATIONS
 
-                case short n when (0b11_1110_0000_0000 <= n && n <= 0b11_1111_1111_1111):
+                case short opCodeRange when (0b11_1110_0000_0000 <= opCodeRange && opCodeRange <= 0b11_1111_1111_1111):
                     _commandSet._literalCommands.ADDLW(literal);
                     break;
 
-                case short n when (0b11_1001_0000_0000 <= n && n <= 0b11_1001_1111_1111):
+                case short opCodeRange when (0b11_1001_0000_0000 <= opCodeRange && opCodeRange <= 0b11_1001_1111_1111):
                     _commandSet._literalCommands.ANDLW(literal);
                     break;
 
-                case short n when (0b10_0000_0000_0000 <= n && n <= 0b10_0111_1111_1111):
+                case short opCodeRange when (0b10_0000_0000_0000 <= opCodeRange && opCodeRange <= 0b10_0111_1111_1111):
                     _commandSet._literalCommands.CALL(target);
                     break;
 
@@ -139,15 +137,15 @@ namespace Simulator.Models.Controls
                     _commandSet._literalCommands.CLRWTD();
                     break;
 
-                case short n when (0b10_1000_0000_0000 <= n && n <= 0b10_1111_1111_1111):
+                case short opCodeRange when (0b10_1000_0000_0000 <= opCodeRange && opCodeRange <= 0b10_1111_1111_1111):
                     _commandSet._literalCommands.GOTO(target);
                     break;
 
-                case short n when (0b11_1000_0000_0000 <= n && n <= 0b11_1000_1111_1111):
+                case short opCodeRange when (0b11_1000_0000_0000 <= opCodeRange && opCodeRange <= 0b11_1000_1111_1111):
                     _commandSet._literalCommands.IORLW(literal);
                     break;
 
-                case short n when (0b11_0000_0000_0000 <= n && n <= 0b11_0011_1111_1111):
+                case short opCodeRange when (0b11_0000_0000_0000 <= opCodeRange && opCodeRange <= 0b11_0011_1111_1111):
                     _commandSet._literalCommands.MOVLW(literal);
                     break;
 
@@ -155,7 +153,7 @@ namespace Simulator.Models.Controls
                     _commandSet._literalCommands.RETFIE();
                     break;
 
-                case short n when (0b11_0100_0000_0000 <= n && n <= 0b11_0111_1111_1111):
+                case short opCodeRange when (0b11_0100_0000_0000 <= opCodeRange && opCodeRange <= 0b11_0111_1111_1111):
                     _commandSet._literalCommands.RETLW(literal);
                     break;
 
@@ -167,11 +165,11 @@ namespace Simulator.Models.Controls
                     _commandSet._literalCommands.SLEEP();
                     break;
 
-                case short n when (0b11_1100_0000_0000 <= n && n <= 0b11_1101_1111_1111):
+                case short opCodeRange when (0b11_1100_0000_0000 <= opCodeRange && opCodeRange <= 0b11_1101_1111_1111):
                     _commandSet._literalCommands.SUBLW(literal);
                     break;
 
-                case short n when (0b11_1010_0000_0000 <= n && n <= 0b11_1010_1111_1111):
+                case short opCodeRange when (0b11_1010_0000_0000 <= opCodeRange && opCodeRange <= 0b11_1010_1111_1111):
                     _commandSet._literalCommands.XORLW(literal);
                     break;
             }
